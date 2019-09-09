3c892bec110d9d9dc8d61274879cddf28e948989
linh <tuan-linh.tran@ant-tech.eu>
2019-09-09 09:50:39 +0700

    impl connect web3

--- App.js
+++ App.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import './global'
+import React, { useEffect } from 'react'
+import Web3 from 'web3'
+import { StyleSheet, Text, View } from 'react-native'
+import Web3FusionExtend from 'web3-fusion-extend'
+import WebsocketProvider from './ether-socket-provider'
+
+let web3
+let fusion
+
+function refreshProvider(providerUrl) {
+  let retries = 0
+
+  const retry = (event) => {
+    if (event) {
+      console.log('Web3 provider disconnected or errored.')
+      retries += 1
+
+      if (retries > 5) {
+        console.log(`Max retries of 5 exceeding: ${retries} times tried`)
+        return setTimeout(refreshProvider, 5000)
+      }
+    } else {
+      console.log(`Reconnecting web3 provider`)
+      refreshProvider(providerUrl)
+    }
+
+    return null
+  }
+
+  const provider = new WebsocketProvider(providerUrl)
+
+  provider.on('end', () => retry())
+  provider.on('error', () => retry())
+
+  console.log('New Web3 provider initiated')
+
+  web3 = new Web3(provider)
+  fusion = Web3FusionExtend.extend(web3)
+
+  return provider
+}
 
 export default function App() {
-  const double = x => x + '!!!'
+  useEffect(() => {
+    const provider = refreshProvider('wss://testnetpublicgateway1.fusionnetwork.io:10001')
+  }, [])
 
   return (
     <View style={styles.container}>
-      <Text>{'Linh' |> double}</Text>
+      <Text>{'Linh'}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet. create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
